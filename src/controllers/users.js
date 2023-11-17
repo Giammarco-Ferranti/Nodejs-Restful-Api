06051dd5a5353d96aad7c9f62384c1d60fc4e1c2
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2023-11-17 13:36:47 +0100

    aggiunto status code su getuser

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,27 +1,77 @@
 
 import mongoose from 'mongoose';
+import _ from 'lodash';
 import { userModel } from '../models/user-schema.js';
 const ObjectId = mongoose.Types.ObjectId;
 
 
 export const getUser = async (req,res)=>{
+  try {
   const id = req.params.id
+  
+  if(!ObjectId.isValid(id)){
+    res.status(400)
+    res.send("User not found, try adjusting the id")
+    return
+  }
+  
   const userFind = await userModel.findById(id)
-  res.send(userFind)
-  // const userFind = users.find((user)=> user.id === id)
-  // res.send(userFind)
+
+  if(userFind){
+    res.status(200)
+    res.send(userFind)
+  }else{
+    res.status(400)
+    res.send("User not found")
+  }
+   
+    
+  } catch (error) {
+    if(error){
+      res.status(500)
+      res.send(error)
+    }
+  }
 };
 
 
-export const createUser = (req,res)=>{
-  const user = new userModel({
-    name: req.body.name,
-    surname: req.body.surname,
-    // email: req.body.email,
-  })
-  user.save().then(()=>{
-    res.send(`New user create with the name of ${user.name}`)
-  })
+
+export const createUser = async (req,res)=>{
+  try {
+    console.log(req.body)
+
+    // if(_.isEmpty(req.body)){
+    //   res.status(400)
+    //   res.send("Data not correct. Name, surname and email are required.")
+    //   return
+    // }
+
+    console.log(req.body)
+
+    const user = new userModel({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+    })
+
+    if(user == error){
+      console.log(error)
+    }
+    
+    await user.save().then(()=>{
+        res.status(200)
+        res.send(`New user create with the name of ${user.name}`)
+      })  
+  } catch (error) {
+    res.send(error)
+    //  if(error){
+    //   res.status(400)
+    //   res.send(error.name.message)
+    //   res.send(error.surname.message)
+    //   res.send(error.email.message)
+      
+    // }
+  }
 };
 
 
@@ -36,14 +86,10 @@ export const updateUser = async (req,res)=>{
 
   const userFind = await userModel.findByIdAndUpdate(id, data)
   res.send(userFind)
-  // const userFind = userModel.findById(id)
-  // res.send(`${userFind}`)
 };
 
 export const deleteUser = async (req,res)=>{
   const id  = req.params.id
   const userFind = await userModel.findByIdAndDelete(id)
   res.send("User deleted")
-  // users = users.filter((user)=> user.id !== id)
-  // res.send("user has been deleted")
 };
--- src/models/user-schema.js
+++ src/models/user-schema.js
@@ -6,10 +6,9 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema ({
-// _id: {type: ObjectId},
 name: {type: String, required: true},
-surname: {type: String, required: true}
-// email: {type: String, required: true}
+surname: {type: String, required: true},
+email: {type: String, required: true}
 });
 
 export const userModel = mongoose.model('user', userSchema)
